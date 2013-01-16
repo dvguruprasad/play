ee77db5d8bfaba12008d6d25bf054173b6d1619e
Guruprasad <dv.guruprasad@gmail.com>
2013-01-16 22:14:27 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Play::Application.config.secret_token = 'aa8597b25949c901a4c072d60fd70355e3a0ba685c487150da0556de81b328b4e1edaf7b4a75c6fc159c2e962320b8eee2eb52f8d8e1a7c2bfc0990477cf4dd3'
